e0a5c8a76c649b05dce6521785db97cdfcc6b9a7
Мялин Валентин <valic.my@gmail.com>
2015-12-31 01:13:10 +0200

    table view вывод core data

--- QRCodeGen/ViewControllerTable.swift
+++ QRCodeGen/ViewControllerTable.swift
@@ -12,7 +12,7 @@ import CoreData
 class ViewControllerTable: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
     @IBOutlet var tableView: UITableView!
-  
+    var tickets = [NSManagedObject]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,9 +38,7 @@ class ViewControllerTable: UIViewController,UITableViewDelegate,UITableViewDataS
         
     }
     let textCellIdentifier = "TextCell"
-    var foodNames: [String] = ["Food1","Food2","Food3","Food4","Food5","Food6","Food7","Food8"];
-    var foodImages: [String] = ["image1", "image2", "image3","image4","image5","image6","image7","image8"];
-    
+
     
     
     // MARK:  UITextFieldDelegate Methods
@@ -49,62 +47,36 @@ class ViewControllerTable: UIViewController,UITableViewDelegate,UITableViewDataS
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return foodNames.count
+        return tickets.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(textCellIdentifier, forIndexPath: indexPath) as UITableViewCell
         
         //let row = indexPath.row
-        cell.textLabel?.text = foodNames[indexPath.row]
+      //  cell.textLabel?.text = tickets[indexPath.row]
+        
+        let person = tickets[indexPath.row]
+        cell.textLabel!.text = person.valueForKey("ticketID") as? String
         
         //        cell.textLabel!.text = ticketsList[indexPath.item]
         
         return cell
     }
-    
-    // MARK:  UITableViewDelegate Methods
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        tableView.deselectRowAtIndexPath(indexPath, animated: true)
-        
-        let row = indexPath.row
-        print(foodNames[row])
-    }
+
     
     func fetch() {
-     //   let context = DataController().managedObjectContext
-        let request = NSFetchRequest(entityName: "Tickets")
         
-        // Create a sort descriptor object that sorts on the "title"
-        // property of the Core Data object
-        //let sortDescriptor = NSSortDescriptor(key: "seat", ascending: true)
-        // Set the list of sort descriptors in the fetch request,
-        // so it includes the sort descriptor
-       // request.sortDescriptors = [sortDescriptor]
-       // let predicate = NSPredicate(format: "seat == %@", "027 Повний")
-       // request.predicate = predicate
         do {
-            let results = try DataController().managedObjectContext.executeFetchRequest(request) as! [Tickets]
-            
-            
-            if (results.count > 0) {
-                for result in results {
-                    print(result.ticketID!)
-                }
-            } else {
-                print("No Data")
-            }
-            
+            let request = NSFetchRequest(entityName: "Tickets")
+            tickets = try DataController().managedObjectContext.executeFetchRequest(request) as! [Tickets]
         
         } catch let error as NSError {
             // failure
             print("Fetch failed: \(error.localizedDescription)")
         }
-        
-
-}
-        
-
+        self.tableView.reloadData()
     }
+}
     
 
